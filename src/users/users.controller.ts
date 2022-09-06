24ce7b53a8e2c55e987d43630e4493c06d84ad01
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-09-06 16:26:34 +0900

    Create module, controller, service
    Users, workspaces, dms, channels

---
+++ src/users/users.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Post } from "@nestjs/common";
+
+@Controller('users')
+export class UsersController {
+
+  @Get()
+  getUsers() {
+
+  }
+
+  @Post()
+  postUsers() {
+
+  }
+
+  @Post('login')
+  logIn() {
+
+  }
+
+  @Post('logout')
+  logOut() {
+
+  }
+
+}
