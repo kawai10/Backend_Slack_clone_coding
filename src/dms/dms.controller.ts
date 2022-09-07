14c8c5e62559db903c95b7a835892a83f685649a
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-09-07 13:14:01 +0900

    @Query, @Param 사용법

--- src/dms/dms.controller.ts
+++ src/dms/dms.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from "@nestjs/common";
 
-@Controller('dms')
-export class DmsController {}
+@Controller('api/workspaces/:url/dms')
+export class DmsController {
+  @Get(':id/chats')
+  getChat(@Query() query, @Param() param) {
+    console.log(query.perPage, query.page)
+    console.log(param.id, param.url)
+  }
+
+  @Post(':id/chats')
+  postChat() {
+
+  }
+}
