c2c01733fe303d971912c03565d237f2bc7d598a
Seonghun <77530278+kawai10@users.noreply.github.com>
2022-09-07 13:40:48 +0900

    swagger를 통해 api문서 만들기

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Slack_clone API')
+    .setDescription('The Slack_clone API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+
   await app.listen(3000);
 
   if (module.hot) {
